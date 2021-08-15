9ad03e9f209b088e625b900441434072c90db779
Pavel Suchmann <pavel.suchmann@gmail.com>
2021-08-15 09:31:11 +0200

    indirect symbol

--- reveng.rb
+++ reveng.rb
@@ -120,6 +120,7 @@ class RowInstr < BaseRecord
       "(#{@symbol})"
     else
       arg
+    end
   end
   
   def arg2
@@ -132,6 +133,7 @@ class RowInstr < BaseRecord
       "(#{@symbol})"
     else
       arg
+    end
   end
 
   def decode(data1)
